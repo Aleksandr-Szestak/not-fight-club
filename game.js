005f19e55949ce3bebb18c06b9efc3eff6faab9c
Aleksandr-Szestak <szestak@hotmail.com>
2025-08-19 15:48:01 +0300

    refactor: improve of html,js code

--- game.js
+++ game.js
@@ -2,36 +2,33 @@ const tabButtons = document.querySelectorAll(".tab-btn");
 const tabContents = document.querySelectorAll(".tab-content");
 const tabSelect = document.querySelector(".tab-select");
 
+
+// Клики мышкой по кнопкам табов
+tabButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    activateTab(button.dataset.tab); // tab1...tab5 из атриб. data-tab
+  });
+});
+
+
+// Выбор из select
+tabSelect.addEventListener("change", () => {
+  activateTab(tabSelect.value);  // tab1...tab5 из атриб. value
+});
+
+
+// Убираем или добавляем класс active
 function activateTab(tabId) {
-  // Убираем активные классы
+
   tabButtons.forEach(btn => {
     btn.classList.toggle("active", btn.dataset.tab === tabId);
-    btn.setAttribute("aria-selected", btn.dataset.tab === tabId);
   });
+
   tabContents.forEach(content => {
     content.classList.toggle("active", content.id === tabId);
   });
+
   tabSelect.value = tabId;
 }
 
-
-
-// Клики мышкой
-tabButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    activateTab(button.dataset.tab);
-  });
-
-  // Переключение клавиатурой
-  button.addEventListener("keydown", (e) => {
-    if (e.key === "Enter" || e.key === " ") {
-      e.preventDefault();
-      activateTab(button.dataset.tab);
-    }
-  });
-});
-
-// Выбор из select на мобильных
-tabSelect.addEventListener("change", () => {
-  activateTab(tabSelect.value);
-});
+// ---------------------------------------------------------------------------
