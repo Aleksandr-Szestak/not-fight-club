775671a94195faf71e57db67209eb37e5306ea57
Aleksandr-Szestak <szestak@hotmail.com>
2025-08-22 14:39:51 +0300

    feat: add layout and JS code for Character section

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+// ---------------------------------------------------------------------------
+
 const tabButtons = document.querySelectorAll(".tab-btn");
 const tabContents = document.querySelectorAll(".tab-content");
 const tabSelect = document.querySelector(".tab-select");
@@ -35,7 +37,7 @@ function activateTab(tabId) {
 
 // ---------------------------------------------------------------------------
 document.addEventListener('DOMContentLoaded', () => {
-  
+
   const form = document.getElementById('nameForm');
   const input = document.getElementById('username');
   const button = document.getElementById('saveBtn');
@@ -47,6 +49,13 @@ document.addEventListener('DOMContentLoaded', () => {
     greeting.textContent = `Hello, ${savedName}!`;
     input.value = savedName;
     button.disabled = true; // деактивация кнопки
+
+    tabButtons.forEach(button => {
+      button.disabled = false;
+    });
+
+    form.style.display = 'none';
+
   }
 
   // Сохр. имени по нажатию кнопки
@@ -57,6 +66,14 @@ document.addEventListener('DOMContentLoaded', () => {
       localStorage.setItem('username', name);
       greeting.textContent = `Hello, ${name}!`;
       button.disabled = true;
+
+      tabButtons.forEach(button => {
+        button.disabled = false;
+      });
+
+      form.style.display = 'none';
+
+
     } else {
       alert('Input Character Name');
     }
@@ -72,4 +89,23 @@ const fightButton = document.getElementById("fightBtn");
 fightButton.addEventListener("click", (event) => {
   event.preventDefault();
   activateTab('tab5');
+});
+
+
+
+// ---------------------------------------------------------------------------
+const radios = document.querySelectorAll('.image-radio-hybrid input');
+
+radios.forEach(radio => {
+  radio.addEventListener('change', () => {
+    // убираем выделение у всех
+    document.querySelectorAll('.image-radio-hybrid img').forEach(img => {
+      img.classList.remove('selected');
+    });
+
+    // подсвечиваем выбранное
+    radio.nextElementSibling.classList.add('selected');
+
+    console.log("Выбрана картинка:", radio.value);
+  });
 });
